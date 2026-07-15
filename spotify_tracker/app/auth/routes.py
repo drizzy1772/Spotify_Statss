6d9dbad87e96b822c3ff6c9e6a5cff5547b2e08f
drizzy1772 <bodyafrozen890@gmail.com>
2026-07-16 02:19:46 +0300

    ended with routes

--- spotify_tracker/app/auth/routes.py
+++ spotify_tracker/app/auth/routes.py
@@ -3,20 +3,37 @@
 
 import urllib.parse
 import secrets
-from fastapi import FastAPI, Response,  Cookie, HTTPException, status
+from fastapi import FastAPI, Response,  Cookie, HTTPException, status, Request
 from fastapi.responses import RedirectResponse
+from pydantic import BaseModel
 
 app = FastAPI()
 
-user_state = secrets.token_urlsafe(16)
+@app.get("/login")
+async def login():
+    user_state = secrets.token_urlsafe(16)
 
+    params = {
+        "response_type": "code",
+        "client_id": "1",
+        "scope": "user-read-currently-playing user-read-playback-state",
+        "redirect_uri": "http://127.0.0.1:8000/callback",
+        "state": user_state
+    }
+    
+    query_string = urllib.parse.urlencode(params)
+    print(query_string)
+    
+    base_url = "https://accounts.spotify.com/authorize?"
+    full_url = base_url + query_string
+    print(full_url)   
 
-params = {
-    "response_type": "code",
-    "client_id": "123",
-    "scope": "read",
-    "redirect_uri": "http://127.0.0.1:8000/callback",
-}
+    response = RedirectResponse(url=full_url, status_code=301)
+    response.set_cookie(
+        key="state",
+        value=user_state,
+    )
+    return response
 
 @app.get("/callback")
 async def search_items(

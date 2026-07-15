5ae5cfe2e4575441fb002f2fe02a82bb005fc9b3
drizzy1772 <bodyafrozen890@gmail.com>
2026-07-15 23:54:54 +0300

    router fixing bugs

--- spotify_tracker/app/auth/routes.py
+++ spotify_tracker/app/auth/routes.py
@@ -3,7 +3,7 @@
 
 import urllib.parse
 import secrets
-from fastapi import FastAPI
+from fastapi import FastAPI, Response,  Cookie, HTTPException, status
 from fastapi.responses import RedirectResponse
 
 app = FastAPI()
@@ -12,13 +12,28 @@ user_state = secrets.token_urlsafe(16)
 
 
 params = {
-    "response_type": "json",
+    "response_type": "code",
     "client_id": "123",
     "scope": "read",
     "redirect_uri": "http://127.0.0.1:8000/callback",
-    "state": user_state
 }
 
-@app.get("/redirect")
-async def redirecting_to_new():
-    return RedirectResponse(url="/new-route")
+@app.get("/callback")
+async def search_items(
+    code: str,
+    state: str,
+    state_cookie: str | None = Cookie(alias="state") 
+):
+    if not state_cookie:
+        raise HTTPException(
+            status_code=status.HTTP_400_BAD_REQUEST,
+            detail="Cookie with state is empty bruh"
+        )
+            
+    if not secrets.compare_digest(state, state_cookie):
+        raise HTTPException(
+            status_code = status.HTTP_400_BAD_REQUEST,
+            detail="Mistake of security state is not compare"
+        )
+        
+    return {"code": code, "state": state, "status": "verified"}

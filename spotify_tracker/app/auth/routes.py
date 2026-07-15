e1c9cede39e02a8512585d5f58580d5fff059007
drizzy1772 <bodyafrozen890@gmail.com>
2026-07-15 22:47:54 +0300

    redreicting url

--- spotify_tracker/app/auth/routes.py
+++ spotify_tracker/app/auth/routes.py
@@ -3,6 +3,10 @@
 
 import urllib.parse
 import secrets
+from fastapi import FastAPI
+from fastapi.responses import RedirectResponse
+
+app = FastAPI()
 
 user_state = secrets.token_urlsafe(16)
 
@@ -14,3 +18,7 @@ params = {
     "redirect_uri": "http://127.0.0.1:8000/callback",
     "state": user_state
 }
+
+@app.get("/redirect")
+async def redirecting_to_new():
+    return RedirectResponse(url="/new-route")

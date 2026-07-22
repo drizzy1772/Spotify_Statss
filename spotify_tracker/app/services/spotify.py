3c49c10ee2548b4d58ac86e1ebfd0fa7827c0893
drizzy1772 <bodyafrozen890@gmail.com>
2026-07-22 21:52:14 +0300

    spotify service end

--- spotify_tracker/app/services/spotify.py
+++ spotify_tracker/app/services/spotify.py
@@ -7,7 +7,8 @@
 import httpx
 import base64
 import os
-from fastapi import HTTPexception
+from fastapi import HTTPException, status
+
 
 SPOTIFY_CLIENT_ID = os.getenv("SPOTIFY_CLIENT_ID", "0065574e049146cfa120c8a8c027861f")
 SPOTIFY_CLIENT_SECRET = os.getenv("SPOTIFY_CLIENT_SECRET", "da44283c1de54e12b5fd7c5b65d293f9")
@@ -16,5 +17,49 @@ async def get_spotify_token() -> str:
     
     url = "https://accounts.spotify.com/api/token"
     
-    text = "client_id:client_secret"
-    b64 = base64.b64encde(text.encode()).decode()
+    credentials = f"{SPOTIFY_CLIENT_ID}:{SPOTIFY_CLIENT_SECRET}"
+    encoded_credentials = base64.b64encode(credentials.encode("utf-8")).decode(
+        "utf-8"
+    )
+    
+    headers = {
+        "Authorization": f"Basic {encoded_credentials}",
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Accept": "application/json"
+    }
+    
+    data = {
+        "grant_type": "client_credentials"
+    }
+    
+    async with httpx.AsyncClient() as client:
+        response = await client.post(url, headers=headers, data=data)
+        if response.status_code != 200:
+            raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                                detail="status_code != 200")
+        else:
+            data = response.json()
+            token = data["access_token"]
+            return token
+
+async def get_track_metadata(track_id: str) -> dict:
+    token = await get_spotify_token()
+    
+    url = f"https://api.spotify.com/v1/tracks/{track_id}"
+    
+    headers = {
+        "Authorization": f'Bearer {token}'
+    }
+    
+    async with httpx.AsyncClient() as client:
+        response = await client.get(url, headers=headers)
+        if response.status_code != 200:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+        else:
+            data = response.json()
+    
+    return {
+        "name": data.get("name"),
+        "artist": data["artists"][0]["name"] if data.get("artists") else "Unknown",
+        "album_cover": data["album"]["images"][0]["url"] if data.get("album") and data["album"]["images"] else None,
+    }

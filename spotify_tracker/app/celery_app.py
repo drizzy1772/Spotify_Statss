e0f96524cab9777491e51e7cb233dbf206fd39f7
drizzy1772 <bodyafrozen890@gmail.com>
2026-07-24 01:28:59 +0300

    celery_app.py ended

--- spotify_tracker/app/celery_app.py
+++ spotify_tracker/app/celery_app.py
@@ -3,19 +3,26 @@
 
 from celery import Celery
 from celery.schedules import crontab
+import os
 
+
+redis_url = os.environ.get("REDIS_URL", "redis://localhost:6379/0")
 celery_app = Celery(
-    "spotify_stats",
-    broker="redis://redis:6379/0",
-    backend="redis://redis:6379/0",
-    include=["app.tasks"])
+    "analytics_worker",
+    broker=redis_url,
+    backend=redis_url,
+    include=["app.tasks.pipeline"])
 
+celery_app.conf.beat_schedule = {
+    "task": 'proj.tasks.send_report',
+    'schedule': crontab(hour=0, minute=0)
+}
 
 celery_app.conf.timezone = "Europe/Kyiv"
 
 celery_app.conf.beat_schedule = {
     'sync-stats-every-midnight': {
-        'task': 'app.tasks.run_sync_daily_stats_task',
+        'task': 'app.tasks.pipeline.sync_daily_spotify_stats',
         'schedule': crontab(minute=0, hour=0)
     }
 }
